f5b2691ffcb5ce8982b50703d59f155f7577edf0
unknown <mrgruby@gmail.com>
2022-02-27 19:38:27 +0100

    Removed Cached

---
+++ CommandsService/Data/CommandsRepo.cs
@@ -0,0 +1,42 @@
+﻿using CommandsService.Models;
+
+namespace CommandsService.Data
+{
+    public class CommandsRepo : ICommandsRepo
+    {
+        public void CreateCommand(int platformId, Command command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreatePlatform(Platform plaform)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetCommand(int platformId, int commandId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetCommandsForPlatform(int platformId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PlatformExists(int platformId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CommandsService/Data/ICommandsRepo.cs
@@ -0,0 +1,24 @@
+﻿using CommandsService.Models;
+
+namespace CommandsService.Data
+{
+    public interface ICommandsRepo
+    {
+        bool SaveChanges();
+
+        //Platforms
+
+        IEnumerable<Platform> GetAllPlatforms();
+
+        void CreatePlatform(Platform plaform);
+
+        bool PlatformExists(int platformId);
+
+        //Commands
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+
+        IEnumerable<Command> GetCommand(int platformId, int commandId);
+
+        void CreateCommand(int platformId, Command command);
+    }
+}
